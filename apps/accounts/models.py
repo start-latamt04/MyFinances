508b96e3e3e032308eaa40c220d411b22582730e
Giovanna Freitas <giovannafreitas15.gf@gmail.com>
2020-08-21 13:07:09 -0300

    configuração de gastos na página relatorio_despesas

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -8,7 +8,6 @@ class Saldo(models.Model):
     saldo = models.DecimalField(max_digits=10000000, decimal_places=2, default=0)
     meta = models.DecimalField(max_digits=10000000, decimal_places=2, default=0)
     gastos = models.DecimalField(max_digits=10000000, decimal_places=2, default=0)
-
     created_at = models.DateTimeField(auto_now_add=True)
 
 
