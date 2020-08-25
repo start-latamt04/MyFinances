c0e12531c128e856bb7c18761e622eb98b782d62
Vitor <vitorsoaresk@gmail.com>
2020-08-25 10:21:11 -0300

    tirando os bugs e ajustando css

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,13 +1,8 @@
 from django.db import models
-<<<<<<< HEAD
-=======
-from django.contrib.auth import get_user_model
->>>>>>> b5f7c016f9f4c1df5264a2d7ba610211455a8856
 
 
 class Saldo(models.Model):
 
-<<<<<<< HEAD
     saldo = models.DecimalField(max_digits=10000000, decimal_places=2, null=True)
     meta = models.DecimalField(max_digits=10000000, decimal_places=2, null=True)
     gastos = models.DecimalField(max_digits=10000000, decimal_places=2, null=True)
@@ -16,13 +11,4 @@ class Saldo(models.Model):
 
     def __str__(self):
         return self.gastos, self.descricao
-=======
-    saldo = models.DecimalField(max_digits=10000000, decimal_places=2, default=0)
-    meta = models.DecimalField(max_digits=10000000, decimal_places=2, default=0)
-    gastos = models.DecimalField(max_digits=10000000, decimal_places=2, default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.gastos
->>>>>>> b5f7c016f9f4c1df5264a2d7ba610211455a8856
