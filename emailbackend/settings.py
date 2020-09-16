6b6ac097938ab3c7a7d1d4e01bd14fab41d981c7
Vitor <vitorsoaresk@gmail.com>
2020-09-16 11:57:32 -0300

    configuracao heroku

--- emailbackend/settings.py
+++ emailbackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import sys
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/page-one'
 LOGOUT_REDIRECT_URL = '/login'
 AUTH_USER_MODEL = 'accounts.Usuario'
+
+
+django_heroku.settings(locals())
