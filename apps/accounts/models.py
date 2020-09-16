9af2986fe66b3776ce8c433f616855d084e72415
Vitor <vitorsoaresk@gmail.com>
2020-09-16 12:27:21 -0300

    alter field again

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 class Saldo(models.Model):
 
-    saldo = models.DecimalField(max_digits=1000, decimal_places=2, null=True)
-    meta = models.DecimalField(max_digits=1000, decimal_places=2, null=True)
-    gastos = models.DecimalField(max_digits=1000, decimal_places=2, null=True)
+    saldo = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    meta = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    gastos = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     descricao = models.TextField(max_length=40, null=True, default="")
     created_at = models.DateTimeField(auto_now=True)
-    total_gastos = models.DecimalField(max_digits=1000, decimal_places=2, null=True)
+    total_gastos = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
